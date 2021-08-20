eb44bbb2b54573fd5dd2aa9501f2291aea08b3bb
Marcos Henrique <ok-marcos@hotmail.com>
2021-08-19 22:50:33 -0300

    GET /products

---
+++ backend/src/main/java/com/marcos/dsdeliver/repositories/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.marcos.dsdeliver.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.marcos.dsdeliver.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+	
+	List<Product> findAllByOrderByNameAsc();
+}
