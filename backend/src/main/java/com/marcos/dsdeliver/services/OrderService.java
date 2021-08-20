0973cb665c939bc10075c7e637d8aa756a29f52a
Marcos Henrique <ok-marcos@hotmail.com>
2021-08-19 23:16:21 -0300

    GET /orders

---
+++ backend/src/main/java/com/marcos/dsdeliver/services/OrderService.java
@@ -0,0 +1,29 @@
+package com.marcos.dsdeliver.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.marcos.dsdeliver.dto.OrderDTO;
+import com.marcos.dsdeliver.entities.Order;
+import com.marcos.dsdeliver.repositories.OrderRepository;
+
+
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+
+	@Transactional(readOnly = true )
+	public List<OrderDTO> findAll(){
+		List<Order> list = repository.findOrdersWithProducts();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+		
+		
+	}
+}
