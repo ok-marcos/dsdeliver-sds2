84c672eb9f1c3810c00b8c668428f8ec74f5afb6
Marcos Henrique <ok-marcos@hotmail.com>
2021-08-21 18:29:45 -0300

    Add routes and StepsHeader

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 
 import './App.css';
-import Home from './Home';
-import Navbar from './Navbar';
+
+import Routes from './routes';
 
 function App() {
   return (
-    < >
-     <Navbar/>
-     <Home/>
-    </>
+    <Routes/>
   );
 }
 
